94746a6a81fe70d5d907d2f4289d1dbca3511d6b
PedroRS9 <pedroglv98@gmail.com>
2023-03-15 00:08:46 +0000

    Header responsive funcional

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,2 +1,7 @@
 import { loadOver } from "../index.js"
 loadOver("../../html/header.html", document.querySelector('script[src="../scripts/components/header.js"]'))
+setTimeout(function() {
+    var script = document.createElement('script');
+    script.src = '../scripts/hamburger.js';
+    document.body.appendChild(script);
+}, 10); /* SI NO ESPERAS UN POCO NO HAY MANERA DE QUE CARGUE BIEN EL SCRIPT. DESCONOZCO EL MOTIVO. */
