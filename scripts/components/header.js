5970ce08007b3bb993f6270d79608af0330f3223
PedroRS9 <pedroglv98@gmail.com>
2023-03-18 11:57:43 +0000

    Arreglo de la chapuza de esperar 10ms para cargar el script de la hamburguesa

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,7 +1,7 @@
 import { loadOver } from "../index.js"
 loadOver("../../html/header.html", document.querySelector('script[src="../scripts/components/header.js"]'))
-setTimeout(function() {
+document.addEventListener('DOMContentLoaded', function(){
     var script = document.createElement('script');
     script.src = '../scripts/hamburger.js';
     document.body.appendChild(script);
-}, 10); /* SI NO ESPERAS UN POCO NO HAY MANERA DE QUE CARGUE BIEN EL SCRIPT. DESCONOZCO EL MOTIVO. */
+});
