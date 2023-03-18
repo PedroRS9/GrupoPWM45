eaae419280f64fbe8e5e180decbfe7a0fd0c47f7
Victor Zuluaga <vj_626@hotmail.com>
2023-03-18 14:26:27 +0000

    al día pedro

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,2 +1,7 @@
 import { loadOver } from "../index.js"
-loadOver("header.html", document.querySelector('script[src="../scripts/components/header.js"]'))
+loadOver("../../html/header.html", document.querySelector('script[src="../scripts/components/header.js"]'))
+document.addEventListener('DOMContentLoaded', function(){
+    var script = document.createElement('script');
+    script.src = '../scripts/hamburger.js';
+    document.body.appendChild(script);
+});
